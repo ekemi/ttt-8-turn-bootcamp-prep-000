3714ccb2857ca6eb58d01d111b6f22dd095b7fab
edmundo papi <edmundopierre@yahoo.com>
2020-03-11 20:06:30 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,13 @@
+def display_board(board)
+
+puts" #{board[0]} #{board[1] |  | "
+puts "-----------"
+puts "  |   |     "
+puts" -----------"
+puts"   |   |     "
+
+end
+
+def turn(board)
+    puts "Please enter 1-9"
+end
